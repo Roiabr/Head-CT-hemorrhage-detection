66da7aa5588b637d6e78d0f9446b999928cc287f
ChenAsaraf <chen21asr@gmail.com>
2020-07-09 11:37:26 +0300

    Create knn file

---
+++ knn.py
@@ -0,0 +1,10 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# k-NN classifier for image classification
+def knn(trainX, trainY, testX, testY, numNeigh):
+    # n_jobs means number of parallel jobs to run. -1 meansusing all processors
+    model = KNeighborsClassifier(n_neighbors=numNeigh, n_jobs=-1)
+    model.fit(trainX, trainY)
+    acc = model.score(testX, testY)
+    print("raw pixel accuracy: {:.2f}%".format(acc * 100))
